17ed3486cf063132412fab74b6eef509239f19ec
Naguib <moma.naguib@gmail.com>
2025-01-01 20:24:55 +0100

    feat: Enhance NextAuth types by adding accessToken to Session and JWT interfaces

--- app/api/auth/[...nextauth].ts
+++ app/api/auth/[...nextauth].ts
@@ -1,7 +1,25 @@
-import NextAuth from "next-auth";
+import NextAuth, {
+  DefaultSession,
+  Account,
+  NextAuthOptions,
+  Session,
+} from "next-auth";
 import GitHubProvider from "next-auth/providers/github";
+import { JWT } from "next-auth/jwt";
 
-export const authOptions = {
+declare module "next-auth" {
+  interface Session extends DefaultSession {
+    accessToken?: string;
+  }
+}
+
+declare module "next-auth/jwt" {
+  interface JWT {
+    accessToken?: string;
+  }
+}
+
+export const authOptions: NextAuthOptions = {
   providers: [
     GitHubProvider({
       clientId: process.env.GITHUB_ID || "",
@@ -10,14 +28,14 @@ export const authOptions = {
   ],
 
   callbacks: {
-    async jwt({ token, account }) {
+    async jwt({ token, account }: { token: JWT; account: Account | null }) {
       if (account) {
         token.accessToken = account.access_token;
       }
       return token;
     },
 
-    async session({ session, token }) {
+    async session({ session, token }: { session: Session; token: JWT }) {
       session.accessToken = token.accessToken;
       return session;
     },
